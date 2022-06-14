8f13921caaaa430299cd0e6d1f90dd2c7751d559
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-14 15:21:16 +0300

    commit tree e892acdffa9a5f314fdee31ca6e107a81e1d8087

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit',(event)=>{
+      event.preventDefault();
+    //   const input = event.target.children[1].value
+    //   console.log(input)
+    const input = document.querySelector('input#searchByID').value
+
+    fetch(`http://localhost:3000/movies/${input}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+        const title = document.querySelector('section#movieDetails h4')
+        const paragraph = document.querySelector('section#movieDetails p')
+        title.textContent = data.title
+        paragraph.textContent = data.summary
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
